ceccfa97e5d63e006c097540e6e97013fc63a798
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-13 14:18:58 +0700

    Add the structure of the JSON and create needed classes. Gradle check : pass:)

--- src/main/java/space/harbour/java/hw4/Movies.java
+++ src/main/java/space/harbour/java/hw4/Movies.java
@@ -1,21 +1,119 @@
 package space.harbour.java.hw4;
 
-public class Movies {
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+
+public class Movies implements Jsonable {
     String title;
     Integer year;
     String released;
     Integer runtime;
     String[] genres;
     Director director;
-    Writer[] writer;
+    Writer[] writers;
+    Actor[] actors;
+    String plot;
+    String[] languages;
+    String[] countries;
+    String awards;
+    String poster;
+    Rating[] ratings;
 
-    public static class Director {
+
+    public static class Director implements Jsonable {
         String name;
+
+        @Override
+        public String toString() {
+            return null;
+        }
+
+        @Override
+        public JsonObject toJsonObject() {
+            return null;
+        }
+
+        @Override
+        public String toJsonString() {
+            return this.toJsonObject().toString();
+        }
+
+        
+
     }
 
-    public static class Writer {
+    public static class Writer implements Jsonable {
         String name;
         String type;
+
+        @Override
+        public String toString() {
+            return null;
+        }
+
+        @Override
+        public JsonObject toJsonObject() {
+            return null;
+        }
+
+        @Override
+        public String toJsonString() {
+            return this.toJsonObject().toString();
+        }
+    }
+
+    public static class Actor implements Jsonable {
+        String name;
+        String as;
+
+        @Override
+        public String toString() {
+            return null;
+        }
+
+        @Override
+        public JsonObject toJsonObject() {
+            return null;
+        }
+
+        @Override
+        public String toJsonString() {
+            return this.toJsonObject().toString();
+        }
+    }
+
+    public static class Rating implements Jsonable {
+        String source;
+        String value;
+        int votes;
+
+        @Override
+        public String toString() {
+            return null;
+        }
+
+        @Override
+        public JsonObject toJsonObject() {
+            return null;
+        }
+
+        @Override
+        public String toJsonString() {
+            return this.toJsonObject().toString();
+        }
+    }
+
+    @Override
+    public JsonObject toJsonObject() {
+        return null;
+    }
+
+    @Override
+    public String toJsonString() {
+        return this.toJsonObject().toString();
     }
 
 }
